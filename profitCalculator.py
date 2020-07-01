13fad4e387a4aee9063bc9319db495cf526ccea8
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-01 13:39:38 +0100

    remove unnecessary semi-colons

--- profitCalculator.py
+++ profitCalculator.py
@@ -1,24 +1,24 @@
 def profitCalculator(arr):
-  sell_price = 0;
-  buy_price = 0;
-  max_profit = -1;
-  change_price = True;
+  sell_price = 0
+  buy_price = 0
+  max_profit = -1
+  change_price = True
 
   for i in range(0, len(arr)-1 ):
-    sell_price = arr[i+1];
+    sell_price = arr[i+1]
 
     if(change_price):
       buy_price = arr[i]
 
     if(sell_price < buy_price):
-      change_price = True;
-      continue;
+      change_price = True
+      continue
     else:
-      temp = sell_price - buy_price;
+      temp = sell_price - buy_price
       if(temp > max_profit):
-        max_profit = temp;
-        change_price = False;
+        max_profit = temp
+        change_price = False
 
-  return max_profit;
+  return max_profit
 
 print(profitCalculator([44, 30, 24, 32, 35, 30, 40, 38, 15]))
