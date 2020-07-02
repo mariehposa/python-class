7949343cce5949dd046d8354dc182d1b740b6abc
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-02 08:40:53 +0100

    remove unnecessary semi-colons

--- elements.py
+++ elements.py
@@ -1,17 +1,17 @@
 def element(arr1, arr2):
-  common_items = [];
-  different_items = [];
+  common_items = []
+  different_items = []
 
   for i in range(0, len(arr1)):
     if(arr1[i] in arr2):
-      common_items.append(arr1[i]);
+      common_items.append(arr1[i])
     else:
-      different_items.append(arr1[i]);
+      different_items.append(arr1[i])
 
   for j in range(0, len(arr2)):
     if(arr2[j] not in arr1):
-      different_items.append(arr2[j]);
+      different_items.append(arr2[j])
 
-  return common_items, different_items;
+  return common_items, different_items
 
 print(element(['a', 'c', 'b', 'o'], ['a', 'b', 'f', 'h']))
