cef06d3ad03526c91f92d046070d2e0d503e09ee
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-03 19:41:28 +0100

    wip

--- hash.py
+++ hash.py
@@ -8,3 +8,16 @@ print(mydict)
 print(mydict['name'])
 print(mydict2['name'])
 print('###')
+
+print(mydict2.keys())
+print(mydict2.values())
+print('###')
+
+for x,y in mydict.items():
+  print(x, ':', y)
+print('###')
+
+mydict['school'] = 'lambda'
+mydict['id'] = '1234'
+print(mydict)
+print('###')
