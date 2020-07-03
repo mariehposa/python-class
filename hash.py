69a9ba53df6981612b8cb161231199750a2068b7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-03 19:40:29 +0100

    wip

--- hash.py
+++ hash.py
@@ -1,3 +1,10 @@
 hashName = "mariam"
 print(hash(hashName))
 print('###')
+
+mydict = dict(name = 'mariam', id = '123', personality = 'enfj')
+mydict2 = {'name': 'adedeji', 'id': '123'}
+print(mydict)
+print(mydict['name'])
+print(mydict2['name'])
+print('###')
