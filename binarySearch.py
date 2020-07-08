91a73296185d9c792725099d233879a93cb61959
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-08 15:05:31 +0100

    wip

--- binarySearch.py
+++ binarySearch.py
@@ -4,4 +4,16 @@ def search (arr, start, end, key):
       
     mid = (start + end) // 2
 
-    
+    if arr[mid] == key: 
+      return mid 
+   
+    if arr[start] <= arr[mid]: 
+      if key <= arr[mid] and key >= arr[start]: 
+        return search(arr, start, mid-1, key) 
+      return search(arr, mid+1, end, key) 
+
+    if key >= arr[mid] and key <= arr[end]: 
+      return search(arr, mid+1, end, key)
+
+    return search(arr, start, mid-1, key)
+
