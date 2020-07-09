7ef59da7df630bc5894ac1d1fad8bd9118c375ab
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-09 14:17:35 +0100

    create function to get key

--- LRUCache.py
+++ LRUCache.py
@@ -1,4 +1,11 @@
 class LRUCache:
     def __init__(self, capacity: int): 
 		self.cache = OrderedDict() 
-		self.capacity = capacity 
+		self.capacity = capacity
+
+    def get(self, key: int) -> int: 
+		if key not in self.cache: 
+			return -1
+		else: 
+			self.cache.move_to_end(key) 
+			return self.cache[key] 
