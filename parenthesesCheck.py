5c69e9a9610aedd9cc10315c9a309fdd617e7bf9
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-09 13:48:55 +0100

    wip

--- parenthesesCheck.py
+++ parenthesesCheck.py
@@ -1,2 +1,23 @@
 def parenthesesCheck(characters):
+  stack = []
+
+  for character in characters:
+    if character in ['{', '[', '(']:
+      stack.append(character)
+    else:
+      if not stack:
+        return False
+        
+      current_character = stack.pop()
+
+      if current_character == '{':
+        if character != '}':
+          return False
+      if current_character == '[':
+        if character != ']':
+          return False
+      if current_character == '(':
+        if character != ')':
+          return False
+
   
