857f30357ec50d8a3912a356c14089fbece47d3d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-10 12:06:27 +0100

    create sort function

--- LRUCache.py
+++ LRUCache.py
@@ -2,8 +2,8 @@ from collections import OrderedDict
 
 class LRUCache:
     def __init__(self, capacity: int): 
-		self.cache = OrderedDict() 
-		self.capacity = capacity
+        self.cache = OrderedDict()
+        self.capacity = capacity
 
     def get(self, key: int) -> int: 
 		if key not in self.cache: 
---
+++ sortByPattern.py
@@ -0,0 +1,1 @@
+def sortByPattern(str, pattern):
