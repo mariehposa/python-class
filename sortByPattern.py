f235c182be8188ae1dd10732a85d546777e9ba7a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-10 12:07:27 +0100

    wip

--- sortByPattern.py
+++ sortByPattern.py
@@ -1,1 +1,10 @@
 def sortByPattern(str, pattern):
+    store = ""
+
+    for i in pattern:
+        if i in str:
+        for char in str:
+            if char == i:
+            store += char
+
+    return store
