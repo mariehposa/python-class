22a98f2641c7345768ba03e43b6d4bf9c8fc106f
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-12 20:55:45 +0100

    get Ancestors

--- binaryTree.py
+++ binaryTree.py
@@ -4,3 +4,16 @@ class binaryTree:
     self.left = None
     self.right = None
 
+def getAncestors(root, target):
+  if root == None:
+    return False
+  
+  if root.data == target:
+    return True
+
+  if (getAncestors(root.left, target) or getAncestors(root.right, target)):
+    print(root.data)
+    return True
+  else:
+    return False
+
