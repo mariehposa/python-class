663441c895ebc7b93392b12f0fcc29f323e82f5a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-13 14:13:24 +0100

    optimise code

--- binarySearch.py
+++ binarySearch.py
@@ -1,20 +1,18 @@
 def search (arr, start, end, key):
-    if key not in arr:
-      return ("Not Found") 
-      
-    mid = (start + end) // 2
+  if key not in arr:
+    return ("Not Found") 
+    
+  mid = (start + end) // 2
 
-    if arr[mid] == key: 
-      return mid 
-   
-    if arr[start] <= arr[mid]: 
-      if key <= arr[mid] and key >= arr[start]: 
-        return search(arr, start, mid-1, key) 
-      return search(arr, mid+1, end, key) 
-
+  if arr[mid] == key: 
+    return mid 
+  elif arr[start] <= arr[mid]: 
+    if key <= arr[mid] and key >= arr[start]: 
+      return search(arr, start, mid-1, key) 
+    return search(arr, mid+1, end, key) 
+  elif arr[end] >= arr[mid]:
     if key >= arr[mid] and key <= arr[end]: 
       return search(arr, mid+1, end, key)
-
     return search(arr, start, mid-1, key)
 
 print(search([ 2, 5, 1, 9], 0, 3, 1))
